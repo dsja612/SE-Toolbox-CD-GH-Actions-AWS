4e2265bf8545a6ac41b567c3b5c4c663477c4cf7
Dylan Sng <dsja612@gmail.com>
2023-10-21 14:25:21 +0800

    fix cors in matching

--- backend/matching-service/src/index.ts
+++ backend/matching-service/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "http://localhost:5173",
+    credentials: true,
   }
 });
 
