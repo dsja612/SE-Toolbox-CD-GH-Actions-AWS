8aa74a0526dbb490e6e2bd0efe0896f3b84650c9
Dylan Sng <dsja612@gmail.com>
2023-10-22 15:58:15 +0800

    testt

--- frontend/src/configs.tsx
+++ frontend/src/configs.tsx
@@ -1,11 +1,5 @@
-// export const QUESTIONS_SERVICE_URL = 'http://localhost:8080/questions';
-// export const USERS_SERVICE_URL = "http://localhost:5000/users";
-// export const AUTH_SERVICE_URL = "http://localhost:5000/auth";
-// export const MATCHING_SERVICE_URL = "http://localhost:3000/";
-// export const COLLABORATION_SERVICE_URL = "http://localhost:4000/";
-
-export const QUESTIONS_SERVICE_URL = 'http://question-service-env.eba-4aupdzsq.ap-southeast-1.elasticbeanstalk.com/questions';
-export const USERS_SERVICE_URL = "http://user-service-env.eba-8rgiqpfp.ap-southeast-1.elasticbeanstalk.com/users";
-export const AUTH_SERVICE_URL = "http://user-service-env.eba-8rgiqpfp.ap-southeast-1.elasticbeanstalk.com/auth";
-export const MATCHING_SERVICE_URL = "http://matching-service-env.eba-vxjxwtrp.ap-southeast-1.elasticbeanstalk.com/";
-export const COLLABORATION_SERVICE_URL = "http://collaboration-service-env.eba-mmdefudd.ap-southeast-1.elasticbeanstalk.com/";
+export const QUESTIONS_SERVICE_URL = process.env.QUESTIONS_SERVICE_URL || 'http://localhost:8080/questions';
+export const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL || "http://localhost:5000/users";
+export const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || "http://localhost:5000/auth";
+export const MATCHING_SERVICE_URL = process.env.MATCHING_SERVICE_URL || "http://localhost:3000/";
+export const COLLABORATION_SERVICE_URL = process.env.COLLABORATION_SERVICE_URL || "http://localhost:4000/";
