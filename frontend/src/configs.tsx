2c0eb1d2dfdb7c32b4df27cae8e3dcc67f6af6a5
Dylan Sng <dsja612@gmail.com>
2023-10-22 16:07:45 +0800

    fix vite env errors

--- frontend/src/configs.tsx
+++ frontend/src/configs.tsx
@@ -1,5 +1,5 @@
-export const QUESTIONS_SERVICE_URL = process.env.QUESTIONS_SERVICE_URL || 'http://localhost:8080/questions';
-export const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL || "http://localhost:5000/users";
-export const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || "http://localhost:5000/auth";
-export const MATCHING_SERVICE_URL = process.env.MATCHING_SERVICE_URL || "http://localhost:3000/";
-export const COLLABORATION_SERVICE_URL = process.env.COLLABORATION_SERVICE_URL || "http://localhost:4000/";
+export const QUESTIONS_SERVICE_URL = import.meta.env.VITE_QUESTIONS_SERVICE_URL || 'http://localhost:8080/questions';
+export const USERS_SERVICE_URL = import.meta.env.VITE_USERS_SERVICE_URL || "http://localhost:5000/users";
+export const AUTH_SERVICE_URL = import.meta.env.VITE_AUTH_SERVICE_URL || "http://localhost:5000/auth";
+export const MATCHING_SERVICE_URL = import.meta.env.VITE_MATCHING_SERVICE_URL || "http://localhost:3000/";
+export const COLLABORATION_SERVICE_URL = import.meta.env.VITE_COLLABORATION_SERVICE_URL || "http://localhost:4000/";
